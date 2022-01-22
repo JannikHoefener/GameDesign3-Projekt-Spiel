12b962405efa831e275bae1e3e36633cb2da63a2
Soeren Helms <soeren.helms@repronik.it>
2022-01-22 18:56:21 +0100

    added Robot assets added BodyGuard assets added timer function

--- Heftiges Ballergame/Assets/Scripts/gameui.cs
+++ Heftiges Ballergame/Assets/Scripts/gameui.cs
@@ -8,20 +8,49 @@ public class gameui : MonoBehaviour
     public Text ScoreText;
     public int score = 0;
     public Text TimeText;
-    public int timelimit = 300;
+    public float timeRemaining = 10;
+    
+    public bool timerIsRunning = true;
+    //public int currentTime = 0; // contains current time left
 
     // Start is called before the first frame update
     void Start()
     {
         // call setScore to set the displayed score to 0
         setScore(0); 
-        setTime(timelimit);
+        timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                setTime(timeRemaining);
+            }
+            else 
+            {
+                Debug.Log("Time ran out.");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }    
+        // Sekunden in der Laufzeit ueber DeltaTime
+        //timer += Time.deltaTime;
+        //int seconds = (int)(timer % 60);
+
+        // Spielzeit abgelaufen -> Game Over
+        //if (seconds == timelimit)
+        /*{
+            gameRunning = false; // Spiel gestoppt
+        }
+        while (gameRunning) 
+        {
+            setTime(timelimit-seconds);
+        }*/
     }
 
     void setScore(int points)
@@ -39,8 +68,13 @@ public class gameui : MonoBehaviour
         ScoreText.text = score.ToString();
     }
 
-    void setTime(int count)
+    void setTime(float displayTime)
     {
-        TimeText.text = "Time Left: " + count + "s";
+        displayTime += 1;
+        // calculate minutes and seconds to show in UI
+        float minutes = Mathf.FloorToInt(displayTime / 60);
+        float seconds = Mathf.FloorToInt(displayTime % 60);
+        // use string format for fancy output
+        TimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
