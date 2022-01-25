e31e92e0aabfa428137d0a713c4090edc9efe6cf
Soeren Helms <soeren.helms@repronik.it>
2022-01-25 22:25:01 +0100

    fixed guard collider bug #25

---
+++ Heftiges Ballergame/Assets/Scripts/guardCollider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Guard: Hitbox Collision detected");
+
+        /*
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Guard: Spotted Player.");
+            GameStatistics.Instance.EndReason = 2;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Endscreen");
+        }
+        */
+    }
+}
